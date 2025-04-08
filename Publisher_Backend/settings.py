cb3b2e621be89d538fbb93622d9e3ab57bf4382a
Eltrobit <time8off@gmail.com>
2025-04-08 15:10:11 +0300

    Updated settings.py

--- Publisher_Backend/settings.py
+++ Publisher_Backend/settings.py
@@ -31,11 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'simple_history',
+    'corsheaders',
+    'authentication',
+    'travel_routes',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
