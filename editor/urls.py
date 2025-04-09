cec76684eca9fab5a950c566b577574cb7b052f1
Artem <artempaw01mail.ru>
2025-04-09 10:45:12 +0300

    requirements

--- editor/urls.py
+++ editor/urls.py
@@ -3,7 +3,7 @@ from rest_framework.routers import DefaultRouter
 from .views import *
 
 router = DefaultRouter()
-router.register(r'routes', BinaryDocumentViewSet, basename='binary')
+router.register(r'binary', BinaryDocumentViewSet, basename='binary')
 urlpatterns = [
     path('', include(router.urls))
     #path('binary/', BinaryDocumentViewSet.as_view(), name='BinaryDocument')
